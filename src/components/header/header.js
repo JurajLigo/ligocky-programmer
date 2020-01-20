da59248259408bb70f4b0561851f14f01aa83d64
Juraj Ligocky <j.ligocky@gmail.com>
2020-01-20 21:57:53 +0100

    Work styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,28 +15,54 @@ export const Header = () => (
         hashSpy={true}
         smooth={true}
         duration={500}
-        offset={-50}
+        offset={0}
         className="header__link"
         activeClass="header__link--active"
       >
         <FontAwesomeIcon className="header__icon" icon="child" size="lg" />
         <span className="header__text">ABOUT ME</span>
       </ScrollLink>
-      <Link className="header__link">
-        <FontAwesomeIcon className="header__icon" icon="briefcase" size="lg" />
+      <ScrollLink
+        to="work"
+        spy={true}
+        hashSpy={true}
+        smooth={true}
+        duration={500}
+        offset={-50}
+        className="header__link"
+        activeClass="header__link--active"
+      >
+        <FontAwesomeIcon
+          className="header__icon"
+          icon="briefcase"
+          size="lg"
+        />
         <span className="header__text">EXPERIENCES</span>
-      </Link>
-      <Link className="header__link">
-        <FontAwesomeIcon className="header__icon" icon="tools" size="lg" />
+      </ScrollLink>
+      <ScrollLink
+        to="skills"
+        spy={true}
+        hashSpy={true}
+        smooth={true}
+        duration={500}
+        offset={-50}
+        className="header__link"
+        activeClass="header__link--active"
+      >
+        <FontAwesomeIcon
+          className="header__icon"
+          icon="tools"
+          size="lg"
+        />
         <span className="header__text">SKILLS</span>
-      </Link>
+      </ScrollLink>
       <ScrollLink
         to="education"
         spy={true}
         hashSpy={true}
         smooth={true}
         duration={500}
-        offset={-100}
+        offset={-50}
         className="header__link"
         activeClass="header__link--active"
       >
@@ -66,7 +92,7 @@ export const Header = () => (
         hashSpy={true}
         smooth={true}
         duration={500}
-        offset={-50}l
+        offset={-50}
         className="header__link"
         activeClass="header__link--active"
       >
--- src/components/work/work.js
+++ src/components/work/work.js
@@ -18,20 +18,32 @@ export const Work = () => {
       <div className="base-container">
         <h1 className="title">WORK EXPERIENCES</h1>
         <p className="subtitle">My working experiences</p>
+
         <VerticalTimeline>
           <TimelineItem
             headerText="Posam - Project Allianz"
             date="2008 - 2009, Bratislava"
             title="Junior Java programmer"
             text="8 years high school focused on foreign languages"
-            headerStyle="education__header education__header--gymnasium">
-            <p className="timeline-item__text timeline-item__text">
+            headerStyle="work__header work__header--posam"
+          >
+            <h3 className="work__subtitle">Project description</h3>
+            <p className="work__text">
               Information system for evidence various insurances
             </p>
-            <p className="vertical-timeline-element-text timeline-item__text">
-              Implementing server-domain objects based on diagrams, programming, bug fixing on frontend and
-              backend side
+            <h3 className="work__subtitle">My responsibilites</h3>
+            <p className="work__text">
+              Implementing server-domain objects based on diagrams, programming,
+              bug fixing on frontend and backend side
             </p>
+            <h3 className="work__subtitle">Technology stack</h3>
+            <div className="work__technology">
+              <div className="work__technology-item">J2EE</div>
+              <div className="work__technology-item">Wicket</div>
+              <div className="work__technology-item">Wicket</div>
+              <div className="work__technology-item">Hibernate</div>
+              <div className="work__technology-item">CVS</div>
+            </div>
           </TimelineItem>
         </VerticalTimeline>
       </div>
