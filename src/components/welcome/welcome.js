3ababa804d0d62e3d9bff166345d26e69b0e9b12
Juraj Ligocky <j.ligocky@gmail.com>
2020-02-10 23:26:01 +0100

    typing library for live typing headline

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,4 +1,8 @@
 import React from "react"
+import ReactTypingEffect from "react-typing-effect"
+import Typist from "react-typist"
+import Typing from 'react-typing-animation'
+
 import "./welcome.scss"
 
 export const Welcome = () => {
@@ -7,8 +11,12 @@ export const Welcome = () => {
       <div className="welcome__first"></div>
       <div className="welcome__second"></div>
       <div className="welcome__content">
-        <h2 className="welcome__subtitle">I am</h2>
+        <p className="welcome__subtitle">I am</p>
         <h1 className="welcome__title">Juraj Ligocky</h1>
+        <Typing loop={true} cursorClassName="welcome__cursor">
+          <h2 className="welcome__description">Frontend software engineer</h2>
+          <Typing.Reset count={1} delay={5000} />
+        </Typing>
       </div>
     </div>
   )
