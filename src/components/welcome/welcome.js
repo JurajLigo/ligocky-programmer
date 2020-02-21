702848653d18c7f40134e1c688039cddc5be3f75
Juraj Ligocky <j.ligocky@gmail.com>
2020-02-21 22:29:49 +0100

    update welcome to previous version new gray background

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,67 +1,27 @@
 import React from "react"
-import BackgroundImage from "gatsby-background-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Typing from "react-typing-animation"
 
 import "./welcome.scss"
-import { useStaticQuery } from "gatsby"
 
 export const Welcome = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      desktopImage: file(relativePath: { eq: "hero/mac.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 6000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      mobileImage: file(relativePath: { eq: "hero/mac-mobile-2.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 768) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  const sources = [
-    data.desktopImage.childImageSharp.fluid,
-    {
-      ...data.mobileImage.childImageSharp.fluid,
-      media: `(max-width: 768px) and (orientation: portrait)`,
-    },
-  ]
-
   return (
     <div className="welcome">
-      <div className="welcome__background-wrapper">
-        <BackgroundImage
-          fluid={sources}
-          className="welcome__background"
-        />
+      <div className="welcome__first">
       </div>
-      <div className="welcome__section-wrapper">
-        <div className="welcome__wrapper">
-          <div className="welcome__first"></div>
-          <FontAwesomeIcon
-            className="welcome__arrow-down"
-            icon="arrow-circle-down"
-            size="4x"
-          />
-          <div className="welcome__second"></div>
-          <div className="welcome__content">
-            <p className="welcome__subtitle">I am</p>
-            <h1 className="welcome__title">Juraj Ligocky</h1>
-            <Typing loop={true} cursorClassName="welcome__cursor">
-              <h2 className="welcome__description">
-                Frontend software engineer
-              </h2>
-              <Typing.Reset count={1} delay={5000} />
-            </Typing>
-          </div>
-        </div>
+      <FontAwesomeIcon
+        className="welcome__arrow-down"
+        icon="arrow-circle-down"
+        size="4x"
+      />
+      <div className="welcome__second"></div>
+      <div className="welcome__content">
+        <p className="welcome__subtitle">I am</p>
+        <h1 className="welcome__title">Juraj Ligocky</h1>
+        <Typing loop={true} cursorClassName="welcome__cursor">
+          <h2 className="welcome__description">Frontend software engineer</h2>
+          <Typing.Reset count={1} delay={5000} />
+        </Typing>
       </div>
     </div>
   )
